4a2969ff6e6d51d4142d5aa71fee33a9d786fee8
Marco Zietzling <marco.zietzling@gmail.com>
2018-12-07 07:19:24 +0100

    finished day1

--- day01/day01.py
+++ day01/day01.py
@@ -1,10 +1,27 @@
 print("advent of code 2018 - day 1")
 
-result = 0
+# day 1 - part 1
+frequency = 0
 
 with open("input.txt") as file:
     for line in file:
-        result = result + int(line)
+        frequency = frequency + int(line)
 
-print("part 1: " + str(result))
+print("part 1: " + str(frequency))
+
+# day 1 - part 2
+frequency = 0
+frequencies = set()
+loop = True
+
+while loop:
+    with open("input.txt") as file:
+        for line in file:
+            frequency = frequency + int(line)
+            if frequency in frequencies:
+                print("part 2: " + str(frequency))
+                loop = False
+                break
+            else:
+                frequencies.add(frequency)
 
