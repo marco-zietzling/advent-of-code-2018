d3d7c205a4f87496328407f73d20fd5f54677070
Marco Zietzling <marco.zietzling@gmail.com>
2018-12-17 22:51:27 +0100

    more on day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy
 
 print("advent of code 2018 - day 10")
 
@@ -8,35 +9,45 @@ with open("input.txt") as file:
 # day 10 - part 1
 
 
-class Point:
-    x: int
-    y: int
-    vx: int
-    vy: int
-
-    def __init__(self, x: int, y: int, vx: int, vy: int):
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-
-    def __repr__(self):
-        return f"Point({self.x}, {self.y}, {self.vx}, {self.vy})"
+# class Point:
+#     x: int
+#     y: int
+#     vx: int
+#     vy: int
+#
+#     def __init__(self, x: int, y: int, vx: int, vy: int):
+#         self.x = x
+#         self.y = y
+#         self.vx = vx
+#         self.vy = vy
+#
+#     def __repr__(self):
+#         return f"Point({self.x}, {self.y}, {self.vx}, {self.vy})"
 
 
 points = []
+velocities = []
 
 for line in lines:
-    x, y = map(int, line.split("position=<")[1].split(">")[0].split(","))
-    vx, vy = map(int, line.split("velocity=<")[1].split(">")[0].split(","))
-    point = Point(x=x, y=y, vx=vx, vy=vy)
-    points.append(point)
+    (x, y) = map(int, line.split("position=<")[1].split(">")[0].split(","))
+    (vx, vy) = map(int, line.split("velocity=<")[1].split(">")[0].split(","))
+    #point = Point(x=x, y=y, vx=vx, vy=vy)
+    points.append((x, y))
+    velocities.append((vx, vy))
 
 # print(points)
+# print(velocities)
+canvas_size = 64000
+counter = 0
 
+im = numpy.full((canvas_size, canvas_size), 255, dtype=numpy.ubyte)
+for point in points:
+    im[points[0], point[1]] = 0
 
+image = Image.fromarray(im, "L")
+#image.resize([d*10 for d in size[::-1]])
+image.save(f"image{counter:08}.png")
 
-# canvas_size = 56000
 # canvas = [[-1 for x in range(-canvas_size, canvas_size)] for y in range(-canvas_size, canvas_size)]
 
 
