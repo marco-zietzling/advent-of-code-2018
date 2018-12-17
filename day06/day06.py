1969293a89a276cf2be2c93968dd449a8a7aeb45
Marco Zietzling <marco.zietzling@gmail.com>
2018-12-17 16:39:55 +0100

    day 6 part 2

--- day06/day06.py
+++ day06/day06.py
@@ -5,11 +5,11 @@ print("advent of code 2018 - day 6")
 with open("input.txt") as file:
     lines = [line.strip() for line in file]
 
-# day 6 - part 1
+# day 6 - part 1 and 2
 coords = []
 areas = {}
-Coordinate = namedtuple('Coordinate', 'x y')
-Distance = namedtuple('Distance', 'coord_index distance')
+Coordinate = namedtuple("Coordinate", "x y")
+Distance = namedtuple("Distance", "coord_index distance")
 
 for line in lines:
     x, y = line.split(",")
@@ -20,18 +20,23 @@ for line in lines:
 max_x = max(x for (x, _) in coords) + 1
 max_y = max(y for (_, y) in coords) + 1
 
-grid = [[99 for x in range(max_x)] for y in range(max_y)]
+grid = [[-1 for x in range(max_x)] for y in range(max_y)]
+safe_grid = [[0 for x in range(max_x)] for y in range(max_y)]
+safe_grid_size = 0
+
 for current_coord_index in range(len(coords)):
     areas[current_coord_index] = 0
 
 for x in range(max_x):
     for y in range(max_y):
         distances = []
+        total_distance = 0
 
         for current_coord_index in range(len(coords)):
             pos_x = coords[current_coord_index].x
             pos_y = coords[current_coord_index].y
             distance = abs(x - pos_x) + abs(y - pos_y)
+            total_distance += distance
             distances.append(Distance(coord_index=current_coord_index, distance=distance))
 
         distances.sort(key=lambda t: t.distance)
@@ -39,12 +44,14 @@ for x in range(max_x):
         if distances[0].distance != distances[1].distance:
             grid[x][y] = distances[0].coord_index
             areas[distances[0].coord_index] += 1
-        else:
-            grid[x][y] = -1
+
+        if total_distance < 10000:
+            safe_grid[x][y] = 1
+            safe_grid_size += 1
 
 
-with open("result.txt", mode="w") as file:
-    file.write('\n'.join([''.join(['{:3}'.format(i) for i in row]) for row in grid]))
+with open("result1.txt", mode="w") as file:
+    file.write("\n".join([''.join(["{:3}".format(i) for i in row]) for row in grid]))
 
 for x in range(max_x):
     areas[grid[x][0]] = -1
@@ -57,3 +64,8 @@ for y in range(max_y):
 print("part 1: " + str(max(areas.values())))
 # 5975
 
+with open("result2.txt", mode="w") as file:
+    file.write("\n".join([''.join(["{:3}".format(i) for i in row]) for row in safe_grid]))
+
+print("part 2: " + str(safe_grid_size))
+# 38670
